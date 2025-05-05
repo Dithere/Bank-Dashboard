ea0907253b707098b0d2bd3a13afd2fcf2102af8
Aditya Narayan Mishra <dithere10@gmail.com>
2025-05-05 15:47:07 +0530

    Update DBUtil.java

--- src/main/java/cop/DBUtil.java
+++ src/main/java/cop/DBUtil.java
@@ -5,13 +5,14 @@ import java.sql.Connection;
 import java.sql.*;
 
 public class DBUtil {
-	static String host = System.getenv("DB_HOST");
-static String port = System.getenv("DB_PORT");
-static String dbName = System.getenv("DB_NAME");
-static String user = System.getenv("DB_USER");
-static String pass = System.getenv("DB_PASS");
+	static String host = System.getenv("MYSQLHOST");
+static String port = System.getenv("MYSQLPORT");
+static String dbName = System.getenv("MYSQLDATABASE");
+static String user = System.getenv("MYSQLUSER");
+static String pass = System.getenv("MYSQLPASSWORD");
 
-static String url = "jdbc:mysql://" + host + ":" + port + "/" + dbName + "?useSSL=false";
+// Example URL for Railway (SSL enabled)
+static String url = "jdbc:mysql://" + host + ":" + port + "/" + dbName + "?useSSL=true";
 
 
     static {
