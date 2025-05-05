9a043e357f3853ddb6f45d6fd2f83088a4994bc9
Aditya Narayan Mishra <dithere10@gmail.com>
2025-05-05 20:36:09 +0530

    Update DBUtil.java

--- src/main/java/cop/DBUtil.java
+++ src/main/java/cop/DBUtil.java
@@ -1,33 +1,28 @@
 package cop;
 
-
 import java.sql.Connection;
-import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBUtil {
-	String host = System.getenv("DB_HOST");
-String port = System.getenv("DB_PORT");
-String dbName = System.getenv("DB_NAME");
-String user = System.getenv("DB_USER");
-String pass = System.getenv("DB_PASS");
+    static String host = System.getenv("DB_HOST");
+    static String port = System.getenv("DB_PORT");
+    static String dbName = System.getenv("DB_NAME");
+    static String user = System.getenv("DB_USER");
+    static String pass = System.getenv("DB_PASS");
 
-String url = "jdbc:mysql://" + host + ":" + port + "/" + dbName + "?useSSL=false";
-
-
-
+    static String url = "jdbc:mysql://" + host + ":" + port + "/" + dbName + "?useSSL=false";
 
     static {
         try {
-            // Load the MySQL JDBC driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver"); // Load JDBC driver
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
 
     public static Connection getConnection() throws SQLException {
-    	System.out.println("hfufcj");
+        System.out.println("Connecting to DB...");
         return DriverManager.getConnection(url, user, pass);
     }
 }
-
